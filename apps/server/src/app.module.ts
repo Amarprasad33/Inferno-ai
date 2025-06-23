465f2b85665e97da7591d160cc4e2ffffa9a135d
amansubudhi <amansubudhi12@gmail.com>
2025-06-23 18:31:58 +0530

    chore: scaffold auth and user modules

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule { }
