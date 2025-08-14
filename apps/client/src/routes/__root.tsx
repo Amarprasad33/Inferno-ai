5c1303eb6c86183705423cc75cfbb91d4a6bae08
amarprasadpati <amarprasad.pati@inovaare.com>
2025-08-14 18:07:15 +0530

    added shadcn

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -1,26 +1,34 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+// import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <div className="min-h-screen w-full">
-        <div className="p-2 flex gap-2 justify-center border-b border-[#3f3f3f]">
-          <Link to="/" className="text-[#7b7b7b] [&.active]:text-white [&.active]:font-bold ">
-            Home
-          </Link>
-          <Link to="/about" className="text-[#7b7b7b] [&.active]:text-white [&.active]:font-bold">
-            About
-          </Link>
-          <Link to="/exp/infini" className="text-[#7b7b7b] [&.active]:text-white [&.active]:font-bold">
-            Infini
-          </Link>
-          <Link to="/chat/page" className="text-[#7b7b7b] [&.active]:text-white [&.active]:font-bold">
-            Chat
-          </Link>
+        <div className="p-2 flex gap-2 justify-between border-b border-[#3f3f3f]">
+          <div>Inferno</div>
+
+          <div className='flex gap-3'>
+            <Link to="/" className="text-[#7b7b7b] [&.active]:text-white [&.active]:font-bold ">
+              Home
+            </Link>
+            <Link to="/about" className="text-[#7b7b7b] [&.active]:text-white [&.active]:font-bold">
+              About
+            </Link>
+            <Link to="/exp/infini" className="text-[#7b7b7b] [&.active]:text-white [&.active]:font-bold">
+              Infini
+            </Link>
+            <Link to="/chat/page" className="text-[#7b7b7b] [&.active]:text-white [&.active]:font-bold">
+              Chat
+            </Link>
+          </div>
+
+          <div className=''>
+            <button>Sign in</button>
+          </div>
         </div>
         <Outlet />
-        <TanStackRouterDevtools />
+        {/* <TanStackRouterDevtools /> */}
       </div>
     </>
   ),
